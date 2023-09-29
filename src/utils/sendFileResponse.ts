b8d66ba212083b101e178d0101b0a81575d7fbaa
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-09-30 00:51:27 +0200

    togli getFileSync invia path

--- src/utils/sendFileResponse.ts
+++ src/utils/sendFileResponse.ts
@@ -55,11 +55,11 @@ export const sendFileResponse = async (
 
   try {
     console.log("getting file string", path);
-    const string = await getFileString(path);
+    // const string = await getFileString(path);
     // throw new Error();
     console.log("sending response", path);
     return new Response(
-      "string o carmine"
+      path
       //  { headers }
     );
   } catch (error) {
